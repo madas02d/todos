99d2f5a1f6e4d63f26b9d81600d272385f140f1c
madas02d <mb.damascene24@gmail.com>
2024-11-01 14:19:55 +0100

    update todo-localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const readTodos = ()=>{
+    const readMemory = localStorage.getItem('List')
+    if(readMemory){
+      return JSON.parse(readMemory)
+    }else{
+      return []
+    }
+  }
+
+  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState(readTodos)
+  console.log(todos)
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(input!==''){
+    setTodos([...todos, input])
+    setInput('')
+    }
+  }
+  useEffect(()=>{
+    localStorage.setItem('List',JSON.stringify(todos))
+  },[todos])
+
+  const handleClick = () =>{
+    const userConfirmed = window.confirm('Are you sure? This will clear all items!');
+
+  // If the user clicks "OK", proceed to clear localStorage and reset todos
+  if (userConfirmed) {
+    localStorage.clear();
+    setTodos([]);
+  }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-stone-200'>
+      <form onClick={handleSubmit}>
+      <input 
+      type="text" 
+      value={input}
+      onChange={(e)=>setInput(e.target.value)}
+      placeholder='Enter your Todos'
+      />
+      <button type='submit'>Add</button>
+      <button onClick={handleClick}>Clear List</button>
+      
+      </form>
+      <ul>
+        {todos.map((item, idx)=>(
+          <li key={idx}>{item}</li>
+        ))}
+
+      </ul>
+    </div>
   )
 }
 
